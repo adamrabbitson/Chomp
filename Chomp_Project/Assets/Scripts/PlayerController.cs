10ad8e86621de12f5b5295447786816e29f764ac
adamrabbitson <39174595+adamrabbitson@users.noreply.github.com>
2018-05-22 11:03:19 +1000

    2nd version of project

---
+++ Chomp_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rigidBody;
+
+	//Use this for initialization
+	void Start ()
+	{
+		rigidBody = GetComponent();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidBody.AddForce (movement * speed);
+	}
+}
